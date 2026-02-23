b461dcd7d553806f6795bd37cba27eff2b87faf7
joaosantv <contatotrabalhojoaovitor@gmail.com>
2026-02-23 12:14:40 -0300

    Mudança banco de dados

--- prisma/migrations/20260223132007_add_setup/migration.sql
+++
@@ -1,4 +0,0 @@
--- AlterTable
-ALTER TABLE "Profissional" ADD COLUMN "instagram" TEXT;
-ALTER TABLE "Profissional" ADD COLUMN "nomeSalao" TEXT;
-ALTER TABLE "Profissional" ADD COLUMN "telefone" TEXT;
